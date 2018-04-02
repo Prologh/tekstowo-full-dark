66f743ebb9ebcf34d1eed392bf8aecceba26a987
Piotr Wosiek <piotrwosiek@onet.eu>
2018-04-02 15:42:03 +0200

    Add project files.

---
+++ tekstowo-full-dark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tekstowo_full_dark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
